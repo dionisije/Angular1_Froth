1d68ab2e03cd16962f0517f02f41a6cda56c3628
Dick Marjanovic <dick.marjanovic@accenture.com>
2016-07-21 09:58:42 +0100

    Album table added with static data.

--- app/scripts/controllers/discography.js
+++ app/scripts/controllers/discography.js
@@ -8,10 +8,29 @@
  * Controller of the angular1FrothApp
  */
 angular.module('angular1FrothApp')
-  .controller('DiscographyCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('DiscographyCtrl', function ($scope, albumFactory) {
+  	$scope.albums = [
+  {
+    "Album": "20th Century Froth",
+    "Year": 2000,
+    "Catalogue": "DVDCD1"
+  },
+  {
+    "Album": "Froth 2001",
+    "Year": 2001,
+    "Catalogue": "DVDCD2"
+  },
+  {
+    "Album": "Froth! As You Mean To Go On",
+    "Year": 2002,
+    "Catalogue": "DVDCD3"
+  }];
+  	// $scope.albums = albumFactory.getData().then(function(data){
+  	// 	console.dir(data);
+  	// 	return data;
+  	// })
+
+  	$scope.somethingClicked = function(){
+  		console.log('Table row clicked.'); 
+  	}
   });
