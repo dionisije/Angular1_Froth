856dff405b92b994b8f0875b12e3c6cebfff3cf2
Dick Marjanovic <dick.marjanovic@accenture.com>
2016-07-21 11:21:39 +0100

    Table shows dynamic data from simplified album list.

--- app/scripts/controllers/discography.js
+++ app/scripts/controllers/discography.js
@@ -9,26 +9,11 @@
  */
 angular.module('angular1FrothApp')
   .controller('DiscographyCtrl', function ($scope, albumFactory) {
-  	$scope.albums = [
-  {
-    "Album": "20th Century Froth",
-    "Year": 2000,
-    "Catalogue": "DVDCD1"
-  },
-  {
-    "Album": "Froth 2001",
-    "Year": 2001,
-    "Catalogue": "DVDCD2"
-  },
-  {
-    "Album": "Froth! As You Mean To Go On",
-    "Year": 2002,
-    "Catalogue": "DVDCD3"
-  }];
-  	// $scope.albums = albumFactory.getData().then(function(data){
-  	// 	console.dir(data);
-  	// 	return data;
-  	// })
+
+  	albumFactory.getData().then(function(data){
+  		console.dir(data);
+  		$scope.albums = data;
+  	});
 
   	$scope.somethingClicked = function(id){
   		console.log(id + ' row clicked.'); 
